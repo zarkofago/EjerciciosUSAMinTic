7cc17d8cc58c3a69dea09eab01a290a54e89180a
zarkofago <zarkofago@gmail.com>
2021-06-22 10:20:36 -0500

    reto2100%

--- Reto2.py
+++ Reto2.py
@@ -1,6 +1,17 @@
-valores = input()
-valor = valores.split()
-distancia_mts = float(valor[0])
-velocidad_max = float(valor[1])
-tiempo_seg=float(valor[2])
-distanciakm=disnccia
+registro = input()
+registro = registro.split()
+distancia_mts = float(registro[0])
+velocidad_max = float(registro[1])
+tiempo_seg = float(registro[2])
+distancia_km = distancia_mts/1000
+tiempo_hrs = tiempo_seg/3600
+velocidad_final = distancia_km/tiempo_hrs
+porcentaje = (velocidad_max*0.20)+velocidad_max
+if velocidad_max < 0:
+    print("MEDICION ERRONEA")
+elif velocidad_final <= velocidad_max:
+    print("OK")
+elif velocidad_final < porcentaje and velocidad_final > velocidad_max:
+    print("MULTA")
+elif velocidad_final >= porcentaje:
+    print("CURSO SENSIBILIZACION")
